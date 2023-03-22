84cb4371a177ba1b9f7cde9bcc34a103ecf0850a
Pradeep Bhati <bhat.prad@gmail.com>
2023-03-22 17:32:06 -0400

    Commit Final push

--- EmployeesSolution/EmployeesAPI/Controllers/HiringRequestController.cs
+++ EmployeesSolution/EmployeesAPI/Controllers/HiringRequestController.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeesApi.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeesAPI.Controllers;
 
@@ -12,6 +13,7 @@ public class HiringRequestController : ControllerBase
     }
 
     [HttpPost("/hiring-requests")]
+    [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 10)]
     public async Task<ActionResult> AddHiringRequest([FromBody] HiringRequestModel request)
     {
         if (!ModelState.IsValid)
@@ -27,8 +29,17 @@ public class HiringRequestController : ControllerBase
             Status = HiringRequestStatus.PendingDepartment
         };
         _context.HiringRequests.Add(hiringEntity);
-        await _context.SaveChangesAsync(); return Ok(hiringEntity);
+        await _context.SaveChangesAsync();
+        // A post to a collection should return a 201 Created, a Link to the new item, and a copy of that item.
+        return CreatedAtRoute("hiringrequests-get-by-id", new { id = hiringEntity.Id }, hiringEntity);
     }
 
+    [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 10)]
+    [HttpGet("/hiring-requests/{id:int}", Name = "hiringrequests-get-by-id")]
+    public async Task<ActionResult> GetHiringRequest([FromRoute] int id)
+    {
+        var response = await _context.HiringRequests.SingleOrDefaultAsync(r => r.Id == id);
+        return response is null ? NotFound() : Ok(response);
+    }
 
 }
